b96f2dd8fc2a2e217573248e67b60f41f2102756
Oloja <oloja@kali.kali>
2023-01-23 11:31:31 +0100

    Express app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,47 @@
 const http = require("http");
+const PORT = 5000;
+const { readFileSync } = require("fs");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImg = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
-  console.log(`user hit the server`);
-  res.end('user hit the server');
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/styles.css") {
+    res.writeHead(200, {
+      "content-type": "text/css",
+    });
+    res.write(homeStyles);
+    res.end();
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, {
+      "content-type": "image/svg+xml",
+    });
+    res.write(homeImg);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, {
+      "content-type": "text/javascript",
+    });
+    res.write(homeLogic);
+    res.end();
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.write(`<h1>Page not found</h1>`);
+    res.end();
+  }
 });
 
-server.listen(5000);
+server.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`);
+});
