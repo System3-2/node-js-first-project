5c222a257b7efab31be810ac989be7c690a08228
Oloja <oloja@kali.kali>
2023-01-23 02:49:18 +0100

    Express app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(`user hit the server`);
+  res.end('user hit the server');
+});
+
+server.listen(5000);
