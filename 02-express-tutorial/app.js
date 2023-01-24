8046b7e9bc89885df0f304aa7d62725ee7f07b1e
OlojaMuyiwa <olojam4969@gmail.com>
2023-01-24 14:15:00 +0100

    post v2

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,9 +2,28 @@ const express = require("express");
 let { people } = require('./data');
 const app = express();
 
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+
 app.get('/api/people', (req, res) => {
-  res.status(200).json( { success: true, data: people });
+  res.status(200).json({ success: true, data: people });
 })
+
+app.post('/login', (req, res) => {
+  // console.log(req.body);
+  const { name } = req.body;
+
+  if (name) {
+    res.status(201).send(`Welcome ${name}`)
+  }
+  res.status(401).send('provide credentials please');
+});
+
+app.post('/api/people', (req, res) => {
+  
+});
 
 
 app.listen(5000, () => {
