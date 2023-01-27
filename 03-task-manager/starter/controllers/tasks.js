7cabad27f197a75ea36d5e3fca6585467a2bb9ad
OlojaMuyiwa <olojam4969@gmail.com>
2023-01-27 15:40:35 +0100

    setup

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,9 +1,31 @@
 
 
 const getAllTasks = (req, res) => {
-  res.send('all tasks');
+  res.send('all tasks from controller');
 }
 
+const createTask = (req, res) => {
+  res.json(req.body);
+}
+
+
+const getTask = (req, res) => {
+  res.send('get single task');
+}
+
+const updateTask = (req, res) => {
+  res.send('update task');
+}
+
+const deleteTask = (req, res) => {
+  res.send('delete task');
+}
+
+
 module.exports = {
-  getAllTasks
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
 }
