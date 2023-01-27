27f72f0ce34264f33002e3af2e0c3f905a6d70b2
OlojaMuyiwa <olojam4969@gmail.com>
2023-01-27 13:25:30 +0100

    tats-manager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.send('Task manager app');
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+})
