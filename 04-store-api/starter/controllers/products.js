f7cb3982ea991699c8e9a799f39e1a12b7afc77d
OlojaMuyiwa <olojam4969@gmail.com>
2023-02-11 13:12:39 +0100

    regEX

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,12 +2,16 @@ const { query } = require("express");
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price")
+    .skip(1)
+    .limit(10);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -17,8 +21,42 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regex = /\b(<|>|<=|>=|=|)\b/g;
+    let filters = numericFilters.replace(
+      regex,
+      (match) => `-${operatorMap[match]}-`
+    );
+    console.log(filters)
+  }
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  console.log(limit, page);
+  const skip = (page - 1) * limit;
 
-  const products = await Product.find(queryObject);
+  result = result.skip(skip).limit(limit);
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
